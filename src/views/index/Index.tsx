0b770a148d598c052129f1b306dc4f9dff79fc56
Sneanken <924393527@qq.com>
2019-12-13 13:13:10 +0800

    修改缩进空格

--- src/views/index/Index.tsx
+++ src/views/index/Index.tsx
@@ -6,17 +6,17 @@ interface Props {}
 interface State {}
 
 export default class Index extends Component<Props, State> {
-    constructor(props: Props) {
-        super(props)
-    }
+  constructor(props: Props) {
+    super(props)
+  }
 
-    static defaultProps = {}
+  static defaultProps = {}
 
-    render() {
-        return (
-            <div>
-                <IndexHeader />
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <IndexHeader />
+      </div>
+    )
+  }
 }
